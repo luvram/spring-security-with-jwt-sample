ad64c27e78053a1b512ff2d31b801370f3ec6b9e
Boram Kim <sahul0804@gmail.com>
2021-02-05 17:48:40 +0900

    Add hierarchy

--- src/main/java/com/spring/security/jwt/WebSecurityConfig.java
+++ src/main/java/com/spring/security/jwt/WebSecurityConfig.java
@@ -1,18 +1,22 @@
 package com.spring.security.jwt;
 
 import lombok.AllArgsConstructor;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyUtils;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.FilterInvocation;
+import org.springframework.security.web.access.expression.DefaultWebSecurityExpressionHandler;
+
+import java.util.*;
 
 @EnableWebSecurity
 @AllArgsConstructor
@@ -20,6 +24,20 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private JwtAuthenticationEntryPoint authenticationErrorHandler;
     private JwtAccessDeniedHandler jwtAccessDeniedHandler;
     private TokenProvider tokenProvider;
+
+    @Bean
+    public RoleHierarchy roleHierarchy() {
+        RoleHierarchyImpl roleHierarchy = new RoleHierarchyImpl();
+        Map<String, List<String>> roleHierarchyMap = new HashMap<>();
+        roleHierarchyMap.put("ROLE_ADMIN", Collections.singletonList("ROLE_MANAGER"));
+        roleHierarchyMap.put("ROLE_MANAGER", Collections.singletonList("ROLE_USER"));
+        String roles = RoleHierarchyUtils.roleHierarchyFromMap(roleHierarchyMap);
+
+        roleHierarchy.setHierarchy(roles);
+        // 혹은 아래와 같이 작성할 수 있다.
+        // roleHierarchy.setHierarchy("ROLE_ADMIN > ROLE_MANAGER\nROLE_MANAGER > ROLE_USER");
+        return roleHierarchy;
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -47,6 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers("/admin").hasAuthority("ROLE_ADMIN")
                 .anyRequest().authenticated()
 
+                .expressionHandler(expressionHandler())
                 .and().apply(securityConfigurerAdapter());
     }
 
@@ -58,5 +77,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private JWTConfigurer securityConfigurerAdapter() {
         return new JWTConfigurer(tokenProvider);
+    }
+
+    @Bean
+    public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
+        DefaultWebSecurityExpressionHandler webSecurityExpressionHandler = new DefaultWebSecurityExpressionHandler();
+        webSecurityExpressionHandler.setRoleHierarchy(roleHierarchy());
+        return webSecurityExpressionHandler;
     }
 }
